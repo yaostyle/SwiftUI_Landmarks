45c6ba3ed0e67efd6a79016437777b259519fb64
Christofer <x@tofr.me>
2023-12-17 19:43:25 -0800

    Pass data into child views

--- Landmarks/Views/CircleImge.swift
+++ Landmarks/Views/CircleImge.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct CircleImge: View {
+    var image: Image
+
     var body: some View {
-        Image("turtlerock")
+        image
             .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
             .overlay {
                 Circle().stroke(.white, lineWidth: 4)
@@ -20,5 +22,5 @@ struct CircleImge: View {
 }
 
 #Preview {
-    CircleImge()
+    CircleImge(image: Image("turtlerock"))
 }
---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,52 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Y on 12/17/23.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
+                .frame(height: 300)
+            
+            CircleImge(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                        
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                
+                Text(landmark.description)
+                
+            }
+            .padding()
+
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    LandmarkDetail(landmark: landmarks[0])
+}
